232a876d1f7530a5212957690d8d6cfce46e0ec2
Gamer247 <darshanjsg2003@gmail.com>
2024-11-22 22:51:36 +0530

    updated font

--- dwm/config.h
+++ dwm/config.h
@@ -7,8 +7,8 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Terminus:style=Regular:pixelsize=18:antialias=true:autohint=true" };
-static const char dmenufont[]       = "Terminus:style=Regular:pixelsize=18:antialias=true:autohint=true";
+static const char *fonts[]          = { "monospace:style=Regular:pixelsize=18:antialias=true:autohint=true" };
+static const char dmenufont[]       = "monospace:style=Regular:pixelsize=18:antialias=true:autohint=true";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -58,7 +58,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-tatic const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "st", NULL };
 /* custom commands */
 /* For audio functions, refer:
  * https://www.reddit.com/r/suckless/comments/c64pv8/comment/es69te5/
--- st/config.h
+++ st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Terminus:style=Regular:pixelsize=24:antialias=true:autohint=true";
+static char *font = "monospace:style=Regular:pixelsize=18:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
