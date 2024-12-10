521642410eb14a4eed38eff9e5f5ff8de7c16ee8
Gamer247 <darshanjsg2003@gmail.com>
2024-12-10 20:20:20 +0530

    changed font of dwm and st to more legible font Source Code Pro from adobe insetad of using FreeMono, reason being it is an open source font also available with the package webkittogtk which is required by web-browsers so no new dependency is added

--- st/config.h
+++ st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "monospace:style=Regular:pixelsize=18:antialias=true:autohint=true";
+static char *font = "Source Code Pro:style=Regular:pixelsize=18:antialias=true:autohint=true"; //"monospace:style=Regular:pixelsize=18:antialias=true:autohint=true"
 static int borderpx = 2;
 
 /*
